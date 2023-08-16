e27a1cd58b5f38abb4fced793a86c435d1bbcb46
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2023-08-16 13:54:22 -0300

    feat: cliente side oauth paypal test

---
+++ script.js
@@ -0,0 +1,48 @@
+ function renderPayPalButton() {
+      paypal.Buttons({
+      createOrder: function(data, actions) {
+                return actions.order.create({
+          purchase_units: [{
+            amount: {
+              currency_code: 'USD',
+              value: '19.99',
+            }
+          }]
+        });
+      },
+      onApprove: async function(data, actions) {
+                const captureResponse = await capturePayment(data.orderID); // Implement this function
+        if (captureResponse.success) {
+          displayThankYouMessage(captureResponse.transactionId);
+        } else {
+          handleCaptureFailure()
+        }
+      }
+    }).render('#paypal-button-container');
+  }
+
+  function handleCaptureFailure() {
+    alert('Payment capture failed. Please try again later.');
+  }
+  
+  renderPayPalButton();
+  
+  function displayThankYouMessage(transactionId) {
+    const thankYouDiv = document.getElementById('thank-you');
+    const transactionIdSpan = document.getElementById('transactionId');
+    
+    transactionIdSpan.textContent = transactionId;
+    thankYouDiv.style.display = 'block';
+  }
+  
+  async function capturePayment(orderId) {
+    try {
+      const response = await axios.post('/capture-payment', { orderId });
+      return { success: true, transactionId: response.data.transactionId };
+    } catch (error) {
+      console.error('Error capturing payment:', error);
+      return { success: false };
+    }
+  }  
+
+  
